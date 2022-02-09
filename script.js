0a4ad2484ed073d5236453813c649c2fbd3a3f08
ratzushca <93014061+ratzushca@users.noreply.github.com>
2022-02-08 21:59:24 -0800

    commit 7

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const screen = document.getElementById("calculator");
 
 let memory = "";
 let operator = "";
-
 let previousNumber;
+
+
 
 function inputVal(value) {
     if (isNaN(value)) {
@@ -41,9 +42,11 @@ function numberVal(value) {
     updateScreen();
   }
 
+
 function updateScreen(){
       screen.innerText = memory;
   }
+
 
 
   function calculate(){
@@ -66,6 +69,7 @@ function updateScreen(){
   }
 
 
+  
  function input(){
     document.querySelector(".elements").addEventListener("click",function (e) {
         inputVal(e.target.innerText);
