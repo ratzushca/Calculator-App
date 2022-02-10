daff9fd31f33e96c7594bed6f28908789a0b71fa
ratzushca <93014061+ratzushca@users.noreply.github.com>
2022-02-09 21:58:31 -0800

    commit 9

--- script.js
+++ script.js
@@ -1,75 +1,105 @@
 
 const screen = document.getElementById("calculator");
-
-document.querySelector(".elements").addEventListener("click",function (e) {
-    inputVal(e.target.innerText);
-    });
-
-let memory = "";
-let operator = "";
-let previousNumber;
+const buttons = document.querySelectorAll('.elements');
 
 
+let operator ="";
+let firstOperand = "";
+let secondOperand = "";
 
-function inputVal(value) {
-    if (isNaN(value)) {
-        operandVal(value);
-    }else{
-        numberVal(value);
+buttons.forEach((buttonNr) =>
+buttonNr.addEventListener('click',typeNumbers)
+);
+
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
     }
+    return secondOperand;
 }
 
-function operandVal(value){
-    console.log(value);
-    switch(value){
-        case "+":
-        case "-":
-            operator = value;
-            previousNumber = memory;
-            memory = "";
-            screen.innerText = previousNumber;
-            break;
-        case "=":
-            calculate();
-    }
+function typeNumbers(e){
+let numberSelection = e.target.textContent;
+firstOperand = numberSelection;
+if (isNaN(firstOperand)){
+     screen.value= parseInt(firstOperand)
+ }else{
+screen.value+=firstOperand;
+}
+
 }
 
 
-function numberVal(value) {
-    if (typeof memory === "number") {
-      memory = value;
-    } else {
-      memory = memory + value.toString();
-      console.log(memory)
-    }
-    updateScreen();
-  }
-
-
-function updateScreen(){
-      screen.innerText = memory;
-  }
 
 
 
-  function calculate(){
-      if (operator === null){
-          return;
-      } else{
-          switch (operator){
-              case "+":
-              screen.innerText = parseInt(previousNumber) + parseInt(memory);
-              break;
-              case "-":
-              screen.innerText = parseInt(previousNumber) - parseInt(memory);
-              break;
-          }
-          memory= screen.innerText;
-          console.log(memory);
-          operator = null;
 
-      }
-  }
+
+// function inputVal(value) {
+//     if (isNaN(value)) {
+//         operandVal(value);
+//     }else{
+//         numberVal(value);
+//     }
+// }
+
+// function operandVal(value){
+//     console.log(value);
+//     switch(value){
+//         case "+":
+//         case "-":
+//             operator = value;
+//             previousNumber = memory;
+//             memory = "";
+//             screen.innerText = previousNumber;
+//             break;
+//         case "=":
+//             calculate();
+//     }
+// }
+
+
+// function numberVal(value) {
+//     if (typeof memory === "number") {
+//       memory = value;
+//     } else {
+//       memory = memory + value.toString();
+//       console.log(memory)
+//     }
+//     updateScreen();
+//   }
+
+
+// function updateScreen(){
+//       screen.innerText = memory;
+//   }
+
+
+
+//   function calculate(){
+//       if (operator === null){
+//           return;
+//       } else{
+//           switch (operator){
+//               case "+":
+//               screen.innerText = parseInt(previousNumber) + parseInt(memory);
+//               break;
+//               case "-":
+//               screen.innerText = parseInt(previousNumber) - parseInt(memory);
+//               break;
+//           }
+//           memory= screen.innerText;
+//           console.log(memory);
+//           operator = null;
+
+//       }
+//   }
 
   
     
